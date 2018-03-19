381f4c27b8039589c19fa6650c5b068053af2ed9
Josh Huckle <j.huckle@se16.qmul.ac.uk>
2018-03-19 00:08:17 +0000

    Styled Shopping Basket

---
+++ resources/views/layouts/shoppingCart.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'larvel') }}</title>
+
+    <!-- Styles -->
+  <link href="{{ asset('css/shoppingCart.css') }}" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.0/css/mdb.min.css" rel="stylesheet">
+
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.0/js/mdb.min.js"></script>
+
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'larvel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                        @else
+                            <li style="padding-top: 10px; padding-right: 10px;">
+                                <a href="{{route('product.shoppingCart')}}">
+                                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                    Shopping Cart
+                                    <span class="badge">{{Session::has('cart') ? Session::get('cart')->totalQty : ''}}</span>
+                                </a>
+                            </li>
+                            <div class="btn-group">
+                                <button class="btn btn-outline-danger dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</button>
+                                <div class="dropdown-menu">
+                                    @if(Auth::check())
+                                    <a class="dropdown-item" href="{{ route('user.profile') }}">Profile</a>
+                                    <a class="dropdown-item" href="{{ route('contact.create') }}">Contact Us</a>
+                                        <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    @endif
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </div>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </main>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script>$('.dropdown-toggle').dropdown()</script>
+</body>
+</html>
--- resources/views/shop/shopping-cart.blade.php
+++ resources/views/shop/shopping-cart.blade.php
@@ -1,17 +1,18 @@
-@extends('layouts.app')
+@extends('layouts.shoppingCart')
 @section('title')
   Laravel Shopping Cart
 @endsection
 @section('content')
   @if(Session::has('cart'))
-    <div class="row">
-      <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+  <div class="shoppingCartItems">
+    <div class="ProductContainer">
+      <div class="">
         <ul class="list-group">
             @foreach((array)$products as $product)
             <li class="list-group-item">
-              <span class="badge">{{$product['qty']}}</span>
+            <span class="qty">{{$product['qty']}}</span>
               <strong>{{$product['item']['title']}}</strong>
-              <span class="label label-success">{{$product['price']}}</span>
+              <span class="label label-success">£{{$product['price']}}</span>
               <div class="btn-group">
                 <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">
                   Options <span class="caret"></span>
@@ -30,15 +31,16 @@
         </ul>
       </div>
     </div>
-    <div class="row">
-      <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
-        <strong>Totall: ${{$totalPrice}}</strong>
+    <div id="totalRow" class="row">
+      <div id="totalContainer" class="">
+        <strong id="totalText">Total:</strong>
+        <strong id="totalAmount">£{{$totalPrice}}</strong>
       </div>
     </div>
     <hr>
     <div class="row">
-      <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
-        <a href="{{ route('checkout')}}" type="button" class="btn btn-success">Checkout</a>
+      <div class=""> <!-- col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3 /-->
+        <a href="{{ route('checkout')}}" type="button" class="btn" id="checkoutButton">Checkout</a> <!-- class="btn btn-success" /-->
       </div>
     </div>
   @else
@@ -47,5 +49,6 @@
       <h2>No Items in Cart</h2>
     </div>
   </div>
+</div>
   @endif
 @endsection
