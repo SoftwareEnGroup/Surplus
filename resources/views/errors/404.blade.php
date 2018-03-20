b15f4bf792f59ac8ccd4c11474ff5a827666c6d6
Josh Huckle <j.huckle@se16.qmul.ac.uk>
2018-03-20 12:37:00 +0000

    styled welcome screen. Started on error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,47 +1,35 @@
-<!DOCTYPE html>
-<html>
+<html xmlns = "http://www.w3.org/1999/xhtml">
 
-<head>
-    <title>404</title>
-    <link href = "https://fonts.googleapis.com/css?family=Lato:100" rel = "stylesheet"
-          type = "text/css">
+    <head>
 
-    <style>
-        html, body {
-            height: 100%;
-        }
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            color: #B0BEC5;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-        .title {
-            font-size: 72px;
-            margin-bottom: 40px;
-        }
-    </style>
+        <title>404: Page not found</title>
+        <link rel="stylesheet" href="css/404Style.css" type="text/css">
 
-</head>
-<body>
+    </head>
+    <body>
 
-<div class = "container">
-    <div class = "content">
-        <div class = "title">404 Error</div>
-    </div>
-</div>
+        <div id="header">
+            <a href="#" ><img id="title" src="images/header.png"></a>
+        </div>
 
-</body>
+        <img id="bg" src="images/BG.png">
+
+
+        <div id="content">
+            <h1 id="code"><span>404</span></h1>
+            <h1>Hmm, this isn't right...</h1>
+
+
+        </div>
+
+
+          <a href="javascript:history.back()"><h1 id="back">Go Back</h1></a>
+
+
+
+        <div id="footer">
+        </div>
+
+    </body>
+
 </html>
