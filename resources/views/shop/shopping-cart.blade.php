b4d14393a1909e3f79072b60d0c3ad49f626992a
Josh Huckle <j.huckle@se16.qmul.ac.uk>
2018-03-25 21:18:46 +0100

    Revert "Merge branch 'master' of https://github.com/SoftwareEnGroup/Surplus"
    This reverts commit 505fbeb7915954ccf91166053c24a2f65fb3d9f1, reversing
    changes made to 2c3201192451719526a93b2f4350bacb594d7b56.

--- resources/views/shop/shopping-cart.blade.php
+++ resources/views/shop/shopping-cart.blade.php
@@ -1,59 +1,3 @@
-<!doctype html>
-<head>
-	<style>
-		
-		.checkout{
-			position:relative;
-			bottom:20px;
-			font-weight:bold;
-			font-size:20px;
-		}
-		.qty{
-			position:relative;
-			font-weight:bold;
-			width:40px;
-			height:28px;
-			font-size: 15px !important;
-		}
-		.label{
-			font-size:22px !important;
-		}
-		.list-group-item{
-			
-			font-size:22px !important;
-		}
-		#drop{
-			background:url("http://www.clker.com/cliparts/1/5/c/4/1245687571372410073Soeb_Plain_Arrow_1.svg.med.png") no-repeat !important;
-			background-size:contain !important;
-			position:relative;
-			left:150px;
-			background-size: 50% !important;
-			border-radius: 3px solid transparent important!;
-			transition: all 0.1s ease-in-out important!;
-			width:10px !important;
-			height:40px !important;
-		}
-	
-		#checkoutButton{
-			position:relative;
-			left:785px !important;
-		}
-		#totalText{
-			position:relative;
-			left:-35px !important;
-			font-weight:bold !important;
-			font-size:25px !important;
-		}
-		#totalAmount{
-			position:relative !important;
-			left:-35px !important;
-			font-weight:bold !important;
-			font-size:22px !important;
-			left:0px !important;
-		}
-	</style>
-</head>
-<body>
 @extends('layouts.shoppingCart')
 @section('title')
   Laravel Shopping Cart
@@ -64,14 +8,14 @@
     <div class="ProductContainer">
       <div class="">
         <ul class="list-group">
-		<div class="checkout">Checkout</div>
             @foreach((array)$products as $product)
             <li class="list-group-item">
             <span class="qty">{{$product['qty']}}</span>
               <strong>{{$product['item']['title']}}</strong>
-              <span class="label label-success" id="price">£{{number_format((float)$product['price'], 2, '.', '')}}</span>
+              <span class="label label-success">£{{number_format((float)$product['price'], 2, '.', '')}}</span>
               <div class="btn-group">
-                <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" id="drop"> <span class="caret"></span>
+                <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">
+                  Options <span class="caret"></span>
                 </button>
                 <ul class="dropdown-menu">
                   <li>
@@ -108,5 +52,3 @@
 </div>
   @endif
 @endsection
-</body>
-</html>
