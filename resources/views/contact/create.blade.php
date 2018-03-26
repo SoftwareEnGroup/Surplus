68c3d7b616c1ad4364c2066a38b730acc0d05947
winsome <winsomeyuen@hotmail.com>
2018-03-26 17:48:53 +0100

    Submit Form redirected

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -2,24 +2,29 @@
 
 @section('content')
 <div class="container">
-    {!! Form::open(['route' => 'contact.store']) !!}
+    <div class="card text-white bg-warning mb-3">
+        <div class="card-body">
+            <h3 class="card-title">Contact Form</h3>
+            {!! Form::open(['route' => 'contact.store']) !!}
 
-    <div class="form-group">
-        {!! Form::label('name', 'Your Name') !!}
-        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+            <div class="form-group">
+                {!! Form::label('name', 'Your Name') !!}
+                {!! Form::text('name', null, ['class' => 'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('email', 'E-mail Address') !!}
+                {!! Form::text('email', null, ['class' => 'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::textarea('msg', "Message...", ['class' => 'form-control']) !!}
+            </div>
+
+            <a href="/home" class="btn btn-info">Submit</a>
+            {{--!! Form::submit('Submit', ['class' => 'btn btn-info']) !!--}}
+            {!! Form::close() !!}
+        </div>
     </div>
-
-    <div class="form-group">
-        {!! Form::label('email', 'E-mail Address') !!}
-        {!! Form::text('email', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::textarea('msg', null, ['class' => 'form-control']) !!}
-    </div>
-
-    {!! Form::submit('Submit', ['class' => 'btn btn-info']) !!}
-
-    {!! Form::close() !!}
 </div>
 @endsection
