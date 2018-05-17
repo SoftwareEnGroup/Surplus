0ed69732cc88c56c297ee62a7c5c940c2dcac262
winsome <winsomeyuen@hotmail.com>
2018-05-17 12:25:21 +0100

    update database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', "db4free.net"),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'surplus'),
-            'username' => env('DB_USERNAME', 'winsomeyuen'),
-            'password' => env('DB_PASSWORD', '8449Catto90'),
+            'database' => env('DB_DATABASE', "surplus"),
+            'username' => env('DB_USERNAME', "winsomeyuen"),
+            'password' => env('DB_PASSWORD', "8449Catto90"),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
